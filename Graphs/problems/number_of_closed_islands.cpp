bfecc53f09d127148debbc3f0af73e0d7dbc0e92
HARSHAVARDHA007 <muni.harsha.vardhan@gmail.com>
2023-12-18 21:00:51 +0530

    18th dec2023

--- Graphs/problems/number_of_closed_islands.cpp
+++ Graphs/problems/number_of_closed_islands.cpp
@@ -1,50 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n,m;
-int dx[4] = {-1,1,0,0};
-int dy[4] = {0,0,-1,1};
+int n, m;
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 
-void flood_fill(vector<vector<int>>& grid, int x, int y, vector<vector<int>>& visited) {
-  visited[x][y] =1;
-  if(grid[x][y] == 2) {
-    grid[x][y] = -1;
+bool flood_fill(vector<vector<int>> &grid, int x, int y, vector<vector<int>> &visited)
+{
+  // If co-ordiantes are out of bounds then return false;
+  if (x < 0 || x >= n || y < 0 || y >= m)
+  {
+    return false;
   }
-  for(int i=0; i<4; i++) {
-    int newX = x + dx[i];
-    int newY = y + dy[i];
-    if(newX>=0 && newX < n && newY >=0 && newY < m && grid[newX][newY] == 2 && !visited[newX][newY]) {
-      flood_fill(grid, newX, newY, visited);
+  // if this is 1 then we 
+  if(grid[x][y] == 1 || visited[x][y]) {
+    return true;
+  }
+  visited[x][y] = 1;
+  bool final_res = true;
+  for (int i = 0; i < 4; i++)
+  {
+    // Send out DFS on all 4 sides and if any one comes as false return false
+    int newX = x + dx[i], newY = y + dy[i];
+    bool island = flood_fill(grid, newX, newY, visited);
+    if (!island)
+    {
+      final_res = false;
     }
   }
+  return final_res;
 }
 
-int closedIsland(vector<vector<int>>& grid) {
+int closedIsland(vector<vector<int>> &grid)
+{
   n = grid.size();
   m = grid[0].size();
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<m; j++) {
-      // First row, first col, last row, last col are useless with 0
-      if((i==0 || j==0 || i == n-1 || j == m-1 ) && grid[i][j] == 0) {
-        grid[i][j] = -1;
-      } else {
-        int flag = 0;
-        for(int k=0; k<4; k++) {
-          int x = i + dx[k], y = j + dy[k];
-          if(grid[x][y] == -1) {
-            flag = 1;
-            break;
-          }
-        }
-        // If any of the surroundings is not good then mark as -1.
-        if(flag) {
-          grid[i][j] = -1;
-          vector<vector<int>> visited(n, vector<int>(m,0));
-          // if a new -1 is introduced then have to make previous 2 as -1 as well
-          flood_fill(grid, i,j,visited);
+  vector<vector<int>> visited(n, vector<int>(m, 0));
+  int ans = 0;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      if (!visited[i][j] && grid[i][j] != 1)
+      {
+        bool island = flood_fill(grid, i, j, visited);
+        if (island)
+        {
+          ans++;
         }
       }
     }
   }
-
-    
+  return ans;
 }
